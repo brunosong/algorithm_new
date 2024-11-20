faef19270795cc9ba93648522b58074f9d84551f
brunosong <brunosong@github.com>
2024-11-20 14:10:58 +0900

    backjoon algorithm

---
+++ Algo/소수구하기.py
@@ -0,0 +1,14 @@
+arr = input().split(" ")
+m, n = int(arr[0]), int(arr[1])
+
+primes = [True] * (n + 1)
+primes[0], primes[1] = False, False
+
+for i in range(2, int((n ** 0.5) + 1)):
+    if primes[i]:
+        for j in range(i * i, n + 1, i):
+            primes[j] = False
+
+for i, p in enumerate(primes):
+    if p and i >= m:
+        print(i)
